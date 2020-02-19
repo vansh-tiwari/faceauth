64bb6e4dee7e3fa18bd570a129cd472388985dff
vansh-tiwari <tiwari1998@hotmail.com>
2020-02-19 15:02:43 +0530

    Detected Face

--- create_face_dataset.py
+++ create_face_dataset.py
@@ -5,23 +5,18 @@ import time
 import cv2 as cv
 import os
 
-outputPath = "datasetImages/vansh"
+saveFile = "vansh"
+outputPath = "datasetImages/"+saveFile
 cascadeFile = 'haarcascade_frontalface_default.xml'
 
 detector = cv.CascadeClassifier(cascadeFile)
 
-print("[INFO] starting video stream...")
+print("==> Working on camera...")
 vs = VideoStream(src=0).start()
-# vs = VideoStream(usePiCamera=True).start()
 time.sleep(2.0)
 total = 0
-saveFile = "vansh"
 
-# loop over the frames from the video stream
 while True:
-	# grab the frame from the threaded video stream, clone it, (just
-	# in case we want to write it to disk), and then resize the frame
-	# so we can apply face detection faster
 	frame = vs.read()
 	orig = frame.copy()
 	frame = imutils.resize(frame, width=400)
@@ -31,30 +26,23 @@ while True:
 		cv.cvtColor(frame, cv.COLOR_BGR2GRAY), scaleFactor=1.1, 
 		minNeighbors=5, minSize=(30, 30))
 
-	# loop over the face detections and draw them on the frame
 	for (x, y, w, h) in rects:
 		cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 		# p = os.path.sep.join([outputPath, "{}.png".format(str(total).zfill(5))])
 		# cv.imwrite(p, orig)
 		# total += 1
 
-	# show the output frame
 	cv.imshow("Frame", frame)
 	key = cv.waitKey(1) & 0xFF
-	
-	# if the `k` key was pressed, write the *original* frame to disk
-	# so we can later process it and use it for face recognition
+	saveName = saveFile+"_"+str(total)
 	if key == ord("k"):
-		p = os.path.sep.join([outputPath, "{}.png".format(str(total).zfill(5))])
+		p = os.path.sep.join([outputPath, "{}.jpg".format(saveName.zfill(5))])
 		cv.imwrite(p, orig)
 		total += 1
 
-	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
 		break
 
-# do a bit of cleanup
-print("[INFO] {} face images stored".format(total))
-print("[INFO] cleaning up...")
+print("==> {} face images saved".format(total))
 cv.destroyAllWindows()
 vs.stop()
--- testFile.py
+++ testFile.py
@@ -1,2 +1,16 @@
-import cv2 as cv
 import os
+import cv2 as cv
+import numpy as np
+import recognition as rc
+
+img_test = cv.imread('testImages/test1.png')
+faces, gray_image = rc.Detect(img_test)
+print("==> Detected Faces: ", faces)
+
+for (x,y,w,h) in faces:
+    cv.rectangle(img_test,(x,y),(x+w+5,y+h+5),(120,125,255),thickness=2)
+
+resized = cv.resize(img_test,(1000,700))
+cv.imshow("Detected Face", resized)
+cv.waitKey(0)
+cv.destroyAllWindows
