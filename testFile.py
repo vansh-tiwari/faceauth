a76d71bdb41e916a40ac349bc4526f8f243132c0
vansh-tiwari <tiwari1998@hotmail.com>
2020-03-05 12:46:48 +0530

    Almost image facial recognition done

--- testFile.py
+++ testFile.py
@@ -3,7 +3,7 @@ import cv2 as cv
 import numpy as np
 import recognition as rc
 
-img_test = cv.imread('testImages/test.png')
+img_test = cv.imread('testImages/smoak1.jpg')
 facesDetected, gray_image = rc.Detect(img_test)
 print("==> Detected Faces: ", facesDetected)
 
@@ -17,18 +17,20 @@ print("==> Detected Faces: ", facesDetected)
 
 faces, faceID = rc.labelsTrainData('trainImages')
 faceRecognizer = rc.trainClassifier(faces, faceID)
-faceRecognizer.save('trainingData.yml')
+# faceRecognizer.save('trainingData.yml')
 
-nameDict = {0: 'Veena', 1: 'Vansh'}
+nameDict = {4:'Narendra', 1: 'Vansh', 2: 'Felicity', 3: 'Arrow'}
 
 for face in facesDetected:
     (x,y,w,h) = face
     roiGray = gray_image[y:y+h, x:x+h]
     label, confidence = faceRecognizer.predict(roiGray)
+    # if confidence > 35:
     print("Confidence: {}, Label: {}".format(confidence,label))
     rc.drawRect(img_test, face)
     predictedName = nameDict[label]
     rc.keepText(img_test, predictedName, x, y)
+    # else:print("Need more accurate dataset")
 
 resized = cv.resize(img_test,(1000,700))
 cv.imshow("Detected Face", resized)
